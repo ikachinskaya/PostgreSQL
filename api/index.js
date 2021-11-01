6ab0e456749fcdf41437d866681e10b63a705dfa
Inna <inna.kachinskaya13@gmail.com>
2021-11-01 22:30:18 +0200

    Refactor api

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
 const axios = require("axios").default;
+const queryString = require("query-string");
+
+const {
+  baseURL,
+  get: { users: usersGetObj },
+} = require("../configs/api.json");
 
 const httpClient = axios.create({
-  baseURL: "https://randomuser.me/api",
+  baseURL,
 });
 
-module.exports.loadUsers = async () => {
+module.exports.loadUsers = async (options = {}) => {
+  const queryParams = {
+    ...usersGetObj,
+    ...options,
+  };
+
   const {
-    data: { results },
-  } = await httpClient.get("/?results=100&seed=test&page=1");
-  return results;
+    data: { results: users },
+  } = await httpClient.get(`?${queryString.stringify(queryParams)}`);
+  return users;
 };

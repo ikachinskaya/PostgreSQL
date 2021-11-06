b41947a3f9e3d98e6b779a464a7b6a293582d649
Inna <inna.kachinskaya13@gmail.com>
2021-11-06 17:51:47 +0200

    Add conditionals (coalesce, nullif, greatest)

--- sql/conditionals.sql
+++ sql/conditionals.sql
@@ -71,3 +71,50 @@ SELECT id,
   END AS "APPLE"
 FROM phones;
 ------------------------------------------------
+/*
+ COALESCE возвращает первый попавшийся аргумент, отличный от NULL
+ Если все аргументы =NULL, результатом будет NULL
+ Тип данных должен совпадать
+ */
+--вернет 15
+SELECT coalesce(NULL, NULL, 15);
+---------------------------------
+-- вернет fdgdfg
+SELECT coalesce(NULL, 'fdgdfg');
+---------------------------------
+-- вместо NULL выведет -5000
+INSERT INTO a
+VALUES('TES');
+SELECT x,
+  coalesce(v, -5000) AS v
+FROM a;
+---------------------------------
+/*
+ NULLIF возвращает NULL, если значение 1===значение 2
+ возвращает значение1, если значение 1!==значение 2
+ для обратной операции COALESCE
+ */
+---------------------------------
+-- вернет null
+SELECT nullif(10, 10);
+---------------------------------
+-- вернет null
+SELECT nullif(null, 10);
+------------------------------------------------
+/*
+ GREATEST вернет наибольшее значение из списка
+ LEAST вернет наименьшее значение из списка
+ */
+--вернет 999 для каждого столбца
+SELECT greatest(1, 2, 4, 5, 999, 7, 2, 38)
+FROM users;
+---------------------------------
+-- не работает
+SELECT GREATEST(orders_price.full_price)
+FROM (
+    SELECT sum (phones.price * phones_to_orders.quantity) AS full_price
+    FROM orders
+      JOIN phones_to_orders ON phones_to_orders."orderId" = orders.id
+      JOIN phones ON phones.id = phones_to_orders."phoneId"
+    GROUP BY orders.id
+  ) AS "orders_price"
